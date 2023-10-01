dd720a4404219f0c5e9d9b2eba9221b71b7cc7fa
faysalhosen <faysanhosen7025@gmail.com>
2023-10-01 22:33:51 +0600

    HeroRegister page created

--- src/components/HeroRegister/HeroRegister.jsx
+++ src/components/HeroRegister/HeroRegister.jsx
@@ -1,64 +1,100 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../Firebase/firebase.config";
-
-
-
+import { useState } from "react";
 
 const HeroRegister = () => {
-    
-        const handleRegister = e => {
-            e.preventDefault();
-            console.log('form submitted')
-            const email = e.target.email.value;
-            const password = e.target.password.value;
-            console.log(email, password);
+  const [registerError, setRegisterError] = useState("");
+    const [success, setSuccess] = useState(false);
+   
 
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
 
-
-            createUserWithEmailAndPassword(auth,email,password)
-            .then((result) =>{
-                console.log(result);
-            }).catch((error) =>{
-                console.log(error)
-            })
-        
-        
+    if (password.length < 6) {
+      setRegisterError("Password should be at least 6 characters or longer");
+      //'Password should be at least 6 characters or longer'
+      return;
+    } else if (!/[A-Z]/.test(password)) {
+      setRegisterError(
+        "Your password should have at least one upper case character."
+      );
+      return;
     }
-    
-    return (
-<div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
-        <form onSubmit={handleRegister}>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" name="email" className="input input-bordered" />
+
+  
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
+        setRegisterError("")
+        setSuccess("created succussfullu login")
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password" name="password" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <form onSubmit={handleRegister}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button type="submit"  className="btn btn-primary">
+                  Register
+                </button>
+
+                {registerError && (
+                  <p className="text-red-700"> {registerError}</p>
+                )}
+                {success && <p className="text-green-600"> {success}</p>}
+
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-        </form>
       </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default HeroRegister;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,82 +1,89 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../Firebase/firebase.config";
 import { useState } from "react";
-import { FaEye, FaEyeSlash } from 'react-icons/fa'
-
-
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Register = () => {
-    const [registerError,setRegisterError] = useState({error:false,message:''});
-    const [success,setSuccess] = useState(false);
-    const [showPassword,setShowPassword] = useState(false)
+  const [registerError, setRegisterError] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
+  const handleRegister = (e) => {
+    e.preventDefault();
 
+    // reset error
+    // setRegisterError ({error:false,message:''});
+    // setSuccess({error:false,message:''});
 
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
 
-
-    const handleRegister = e => {
-        e.preventDefault();
-
-         // reset error
-         setRegisterError ({error:false,message:''});
-         setSuccess({error:false,message:''});
- 
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        console.log(email,password);
-
-        if(password.length <6){
-            setRegisterError({error:true,message:'Password should be at least 6 characters or longer'});
-            //'Password should be at least 6 characters or longer'
-            return;
-
-        }
-        
-        else if(!/[A-Z]/.test(password)){
-            setRegisterError({error:true,message:'Your password should have at least one upper case character.'})
-            //'Your password should have at least one upper case character.'
-        
-
-        }
-
-            // create user
-            createUserWithEmailAndPassword (auth, email, password)
-            .then(result => {
-                console.log(result.user);
-                setSuccess('User Created Successfullu')
-            })
-            .catch (error => {
-                console.log(error);
-                setRegisterError({error:true,message:error});
-            })
-        
+    if (password.length < 6) {
+      setRegisterError("Password should be at least 6 characters or longer");
+      //'Password should be at least 6 characters or longer'
+      return;
+    } else if (!/[A-Z]/.test(password)) {
+      setRegisterError(
+        "Your password should have at least one upper case character."
+      );
+      //'Your password should have at least one upper case character.'
     }
 
-    return(
-        <div className='mx-auto md: w-1/2 '>
-            <h2 className="text-3xl">Please Register</h2>
-            <form onSubmit={handleRegister}>
-                <input className='mb-4 w-3/4 py-2 px-4' type="email" placeholder='
-                Email Address' name="email" id="" />
-                <br />
-                <input placeholder='Password' className='mb-4 w-3/4 py-2 px-2' type= {showPassword ? "text" :"password"} name="password" id="" />
-                <br />
-                <input className='mb-4 w-3/4 btn btn-secondary' type="submit" value="register" id="" />
-                <span onClick={ () => setShowPassword(!showPassword)}>Show 
-                {
-                    showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
-                }
-                </span>
-            </form>
+    // create user
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
 
-            {
-                registerError.error && <p className="text-red-700"> {registerError.message}</p>
-            }
-            {
-                success && <p className="text-green-600"> {success}</p>
-            }
+        setSuccess("User Created Successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+        // setRegisterError({error:true,message:error});
+      });
+  };
+
+  return (
+    <div className="mx-auto md: w-1/2 ">
+      <h2 className="text-3xl">Please Register</h2>
+      <form onSubmit={handleRegister}>
+        <input
+          className="mb-4 w-3/4 py-2 px-4"
+          type="email"
+          placeholder="
+                Email Address"
+          name="email"
+          id=""
+        />
+        <br />
+       <div className="relative">
+       <input
+          placeholder="Password"
+          className="mb-4 w-3/4 py-2 px-2"
+          type={showPassword ? "text" : "password"}
+          name="password"
+          id=""
+        />
+        <span className="absoulte" onClick={() => setShowPassword(!showPassword)}>
+        {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+        </span>
+       </div>
+        <div>
+          <div>
+            <input
+              className="mb-4 w-3/4 btn btn-secondary"
+              type="submit"
+              value="register"
+              id=""
+            />
+          </div>
         </div>
-    );
+      </form>
+
+      {registerError && <p className="text-red-700"> {registerError}</p>}
+      {success && <p className="text-green-600"> {success}</p>}
+    </div>
+  );
 };
 
 export default Register;
